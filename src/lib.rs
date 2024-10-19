5b8fe33e20f8abed23f71db1b187c6fa904959f2
filipton <filipton12@gmail.com>
2024-10-19 12:33:27 +0200

    feat: alloc feature (to pass stack as Rc)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
 pub use edge_dhcp::Ipv4Addr;
 use embassy_net::{
     driver::Driver,
@@ -18,8 +21,32 @@ pub mod structs;
 pub type CloseSignal = Signal<CriticalSectionRawMutex, ()>;
 pub static CLOSE_SIGNAL: CloseSignal = Signal::new();
 
+#[cfg(not(feature = "alloc"))]
 pub async fn run_dhcp_server<D: Driver>(
     stack: &'static Stack<D>,
+    config: DhcpServerConfig<'_>,
+    leaser: &'_ mut dyn DhcpLeaser,
+) {
+    let mut rx_buffer = [0; 1024];
+    let mut tx_buffer = [0; 1024];
+    let mut rx_meta = [PacketMetadata::EMPTY; 16];
+    let mut tx_meta = [PacketMetadata::EMPTY; 16];
+    let sock = UdpSocket::new(
+        &stack,
+        &mut rx_meta,
+        &mut rx_buffer,
+        &mut tx_meta,
+        &mut tx_buffer,
+    );
+
+    let mut server = DhcpServer::new(config, leaser, sock);
+    embassy_futures::select::select(server.run(), CLOSE_SIGNAL.wait()).await;
+}
+
+
+#[cfg(feature = "alloc")]
+pub async fn run_dhcp_server<D: Driver>(
+    stack: alloc::rc::Rc<Stack<D>>,
     config: DhcpServerConfig<'_>,
     leaser: &'_ mut dyn DhcpLeaser,
 ) {
